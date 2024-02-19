cf0eacf2a096a19869badedf0dcb3667a77fc44b
KiranDhungana <kirandhungana570@gmail.com>
2024-02-19 17:58:57 +0545

    password hash

--- src/app.js
+++ src/app.js
@@ -1,46 +1,49 @@
-const express = require('express');
-const connection =require('./db.js');
-
-
+const express = require("express");
+const connection = require("./db.js");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    connection.query('SELECT * FROM user', (err, rows) => {
-        if (err) {
-          console.error('Error executing query: ' + err.stack);
-          return;
-        }
-        const dataArray = [];
-        rows.forEach(rows => {
-          const id = rows.id;
-          const name = rows.name;
-          
-          // Push the values into the dataArray
-          dataArray.push({ id, name });
-      });
-      res.send(dataArray);
-
-      });
-});
-
-app.post('/user', (req, res) => {
-  const name =req.body.name;
-  console.log(name);
-  const sql = `INSERT INTO user (name) VALUES ('${name}')`;
-  connection.query(sql,(err, results) => {
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM user", (err, rows) => {
     if (err) {
-        console.error('Error inserting data:', err);
-        return;
+      console.error("Error executing query: " + err.stack);
+      return;
     }
-    res.send('Data inserted successfully');
+    const dataArray = [];
+    rows.forEach((rows) => {
+      const id = rows.id;
+      const name = rows.name;
+
+      // Push the values into the dataArray
+      dataArray.push({ id, name });
+    });
+    res.send(dataArray);
+  });
 });
 
+app.post("/user", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+
+  const password = req.body.password;
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.error("Error hashing password:", err);
+      return;
+    }
+    const sql = `INSERT INTO user (name,email,password) VALUES ('${name}' ,'${email}','${hash}')`;
+    connection.query(sql, (err, results) => {
+      if (err) {
+        console.error("Error inserting data:", err);
+        return;
+      }
+      res.send("Data inserted successfully");
+    });
+  });
 });
-
-
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
